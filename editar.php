5ba03abb19b5d4f34ec912afd3b2d0d6f4626c4b
Jhonathas15 <118564266+Jhonathas15@users.noreply.github.com>
2023-05-29 18:01:33 -0300

    Create editar.php

---
+++ editar.php
@@ -0,0 +1,10 @@
+$endereco = $_POST['Endereco'];
+
+$receber_pedido = "UPDATE livraria 
+SET Nome = $nome, Numero = $numero, Cidade = $cidade, Endereco = $endereco  WHERE id = '$id' ";
+ 
+$cadstrar = mysqli_query($con, $buscar);
+
+header('location:listar.php'); 
+
+?>
